95597e562172b92ae0c8482d1af67b0c3ffc233a
Johan Jönsson <jonsson.johan@outlook.com>
2025-01-12 23:23:44 +0100

    Component tabs.tsx did not support different tab names

--- components/campaign.tsx
+++ components/campaign.tsx
@@ -77,7 +77,7 @@ export default function Campaign({
   return (
     <div>
       {(userRole === 'OWNER' || userRole === 'ARBITRATOR') ? (
-        <Tabs>
+        <Tabs tabTitles={['Details', 'Territories', 'Notes']}>
           <div className="bg-white shadow-md rounded-lg p-4 md:p-6">
             <h1 className="text-2xl font-bold mb-2">{campaign_name}</h1>
             <h2 className="text-gray-600 text-lg mb-4">{campaign_type}</h2>
