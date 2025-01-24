b36c872b5ce6574db9ddf8d41153f41347ac2a2b
Johan Jönsson <jonsson.johan@outlook.com>
2025-01-24 08:33:19 +0100

    Added the vehicles tab on the gang page

---
+++ types/vehicle.ts
@@ -0,0 +1,20 @@
+export interface VehicleProps {
+  id: string;
+  gang_id: string;
+  vehicle_name: string;
+  movement: number;
+  front: number;
+  side: number;
+  rear: number;
+  hull_points: number;
+  handling: number;
+  save: number;
+  body_slots: number;
+  body_slots_occupied: number;
+  drive_slots: number;
+  drive_slots_occupied: number;
+  engine_slots: number;
+  engine_slots_occupied: number;
+  special_rules: string[];
+  cost: number;
+} 
