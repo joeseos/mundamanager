2f32eec24b6105fa34b45bfa98305953e128f4e6
Djidiouf <djidiouf@gmail.com>
2025-01-29 19:45:11 +1100

    Fix the wrapping of long gang names

--- components/ui/breadcrumb.tsx
+++ components/ui/breadcrumb.tsx
@@ -18,7 +18,7 @@ const BreadcrumbList = React.forwardRef<
   <ol
     ref={ref}
     className={cn(
-      "flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground",
+      "flex flex-wrap items-center gap-1 break-words text-sm text-muted-foreground leading-none",
       className
     )}
     {...props}
