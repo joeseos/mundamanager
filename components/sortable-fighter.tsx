6659ffe5d38e554b539f01784cb96fbad401f468
Johan Jönsson <jonsson.johan@outlook.com>
2025-02-13 23:03:08 +0100

    className is not a recognized prop in the FighterCard component

--- components/sortable-fighter.tsx
+++ components/sortable-fighter.tsx
@@ -47,13 +47,14 @@ export function SortableFighter({ fighter, positions, onFighterDeleted }: Sortab
       <div className="absolute left-2 top-1/2 -translate-y-1/2 opacity-50 group-hover:opacity-100 transition-opacity">
         ⋮⋮
       </div>
-      <FighterCard 
-        {...fighter} 
-        name={fighter.fighter_name}
-        type={fighter.fighter_type}
-        disableLink={isDragging}
-        className="pl-8"
-      />
+      <div className="pl-8">
+        <FighterCard 
+          {...fighter} 
+          name={fighter.fighter_name}
+          type={fighter.fighter_type}
+          disableLink={isDragging}
+        />
+      </div>
     </div>
   );
 } 
