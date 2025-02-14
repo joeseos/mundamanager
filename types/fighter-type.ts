99d94db751e5fff4c069b944a324ca63567061de
Johan Jönsson <jonsson.johan@outlook.com>
2025-02-14 10:20:35 +0100

    FighterType interface being used in gang-page-content.tsx doesn't match the one being imported

--- types/fighter-type.ts
+++ types/fighter-type.ts
@@ -1,8 +1,24 @@
 export interface FighterType {
   id: string;
-  fighter_type_id: string;
   fighter_type: string;
   fighter_class: string;
   cost: number;
-  total_cost: number;
+  movement: number;
+  weapon_skill: number;
+  ballistic_skill: number;
+  strength: number;
+  toughness: number;
+  wounds: number;
+  initiative: number;
+  attacks: number;
+  leadership: number;
+  cool: number;
+  willpower: number;
+  intelligence: number;
+  special_rules?: string[];
+  gang_type_id: string;
+  gang_type: string;
+  fighter_class_id?: string;
+  free_skill?: string;
+  total_cost?: number;
 } 
