566d5bd4df3949dfbd265b83d96493ce682b0c7b
Djidiouf <djidiouf@gmail.com>
2025-03-09 04:36:06 +1100

    Added sorting for gang type when creating a new gang

---
+++ utils/gangListRank.ts
@@ -0,0 +1,27 @@
+export const gangListRank: { [key: string]: number } = {
+  "house cawdor": 1,
+  "house delaque": 2,
+  "house escher": 3,
+  "house goliath": 4,
+  "house orlock": 5,
+  "house van saar": 6,
+  //
+  "palanite enforcers": 10,
+  "badzone enforcers": 11,
+  //
+  "corpse grinder cults": 20,
+  "genestealer cults": 21,
+  "helot chaos cults": 22,
+  //
+  "ash waste nomads": 30,
+  "ironhead squat prospectors": 31,
+  "malstrain genestealers": 32,
+  "slave ogryns": 33,
+  "spyre hunting party": 34,
+  "venators": 35,
+  //
+  "underhive outcasts (custom leader)": 40,
+  "underhive outcasts (delegation leader)": 41,
+  "underhive outcasts (elevated leader)": 42,
+  "underhive outcasts (mutant leader)": 43,
+};
