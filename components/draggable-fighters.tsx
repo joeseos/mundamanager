1ef6b1340c3982711571b8d114f14270830b07b2
Djidiouf <djidiouf@gmail.com>
2025-03-23 22:25:14 +1100

    Adjusted the time before a fighter card is draggable by touch on touch device

--- components/draggable-fighters.tsx
+++ components/draggable-fighters.tsx
@@ -25,7 +25,7 @@ export function DraggableFighters({
 
   const touchSensor = useSensor(TouchSensor, {
     activationConstraint: {
-      delay: 1000,
+      delay: 750,
       tolerance: 5,
     },
   });
