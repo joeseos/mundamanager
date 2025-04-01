e86c7067a6c87dea3cbf76a11084de72d3416940
Djidiouf <djidiouf@gmail.com>
2025-04-01 20:00:08 +1100

    Added grouping of weapon profiles by name and display count in fighter card, excluding names starting with '-'

--- components/weapon-table.tsx
+++ components/weapon-table.tsx
@@ -1,6 +1,5 @@
 import React, { useMemo } from 'react';
 import { Weapon, WeaponProfile } from '@/types/weapon';
-import { useEffect } from 'react';
 
 interface WeaponTableProps {
   weapons: Weapon[];
