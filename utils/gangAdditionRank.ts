271e25fc49cfacc5cee02d393eb98e3b71ab85f6
Djidiouf <djidiouf@gmail.com>
2025-04-12 01:35:32 +1000

    Added sorting for Alliances in Gang Addition

--- utils/gangAdditionRank.ts
+++ utils/gangAdditionRank.ts
@@ -8,4 +8,27 @@ export const gangAdditionRank: { [key: string]: number } = {
   "hive scum": 21,
   "house agent": 22,
   "others": 23,
+  //
+  "exotic beast": 30,
+  "special terrain": 31,
+  //
+  "cold traders, smuggler shore party": 40,
+  "fallen house": 41,
+  "imperial imposters": 42,
+  "psi-syndica": 43,
+  "rogue factoria, factoria work gang": 44,
+  //
+  "corpse guild, corpse harvesting party": 50,
+  "guild of coin, toll collectors": 51,
+  "promethium guild, pyromantic conclave": 52,
+  "slave guild, slaver entourage": 53,
+  //
+  "water guild, nautican syphoning delegation": 60,
+  "house catallus, carnival": 61,
+  "house greim, military attaché": 62,
+  "house ko'iron, ministorum delegation": 63,
+  "house ran lo, auditing conclave": 64,
+  "house ty, onmyodo coven": 65,
+  "house ulanti court advisor": 66,
+  "the imperial house, enforcer bodyguards": 67,
 };
