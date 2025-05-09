469eb70697044367d1c1c86d50d9abddb033af28
Johan Jönsson <jonsson.johan@outlook.com>
2025-05-09 09:04:36 +0200

    Fixed the default skill is missing if fighter got a default skill

--- components/gang/fighter-card.tsx
+++ components/gang/fighter-card.tsx
@@ -538,28 +538,31 @@ const FighterCard = memo(function FighterCard({
                 <>
                   <div className="min-w-[0px] font-bold text-sm pr-4 whitespace-nowrap">Skills</div>
                   <div className="min-w-[0px] text-sm break-words">
-                    {[
-                      // Get skills from advancements (old structure)
-                      ...(advancements?.skills ? Object.keys(advancements.skills) : []),
-                      // Get skills from the new structure
-                      ...(skills ? Object.keys(skills) : [])
-                    ]
-                      .filter(Boolean)
-                      .sort((a, b) => a.localeCompare(b))
-                      .join(', ') || 
-                    (free_skill ? (
-                      <div className="flex items-center gap-2 text-amber-700">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          className="w-4 h-4"
-                        >
-                          <path fillRule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
-                        </svg>
-                        Starting skill missing.
-                      </div>
-                    ) : null)}
+                    {(() => {
+                      const skillNames = [
+                        ...(advancements?.skills ? Object.keys(advancements.skills) : []),
+                        ...(skills ? Object.keys(skills) : [])
+                      ].filter(Boolean);
+                      const skillsText = skillNames.sort((a, b) => a.localeCompare(b)).join(', ');
+                      return (
+                        <>
+                          {skillsText}
+                          {free_skill && (
+                            <div className="flex items-center gap-2 text-amber-700 mt-1">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="w-4 h-4"
+                              >
+                                <path fillRule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+                              </svg>
+                              Starting skill missing.
+                            </div>
+                          )}
+                        </>
+                      );
+                    })()}
                   </div>
                 </>
               )}
