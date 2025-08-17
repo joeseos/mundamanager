8be02c0f92b996c360ad54840359430057099ff7
Johan Jönsson <jonsson.johan@outlook.com>
2025-08-17 20:05:50 +0200

    Swap to use turbopack

--- next.config.js
+++ next.config.js
@@ -57,34 +57,6 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.optimization, {
-        minimize: true,
-        splitChunks: {
-          chunks: 'all',
-          minSize: 20000,
-          maxSize: 244000,
-          minChunks: 1,
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-          cacheGroups: {
-            defaultVendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,
-              reuseExistingChunk: true,
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true,
-            },
-          },
-        },
-      });
-    }
-    return config;
-  },
   compress: true,
   generateEtags: true,
   pageExtensions: ['tsx', 'ts'],
