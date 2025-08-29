cb22b6b6d93e6e652166e1c03cbf35c24ca4b6ff
Djidiouf <djidiouf@gmail.com>
2025-08-29 11:54:33 +1000

    Fixed favicon not being at root level

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata = {
   },
   icons: {
     icon: [
-      { url: '/images/favicon.ico', sizes: 'any' },
+              { url: '/favicon.ico', sizes: 'any' },
       { url: '/images/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/images/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
