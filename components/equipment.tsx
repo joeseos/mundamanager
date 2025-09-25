dc43ceb03522ade31b1ca92bb7d40bfe608ba4e8
Johan Jönsson <jonsson.johan@outlook.com>
2025-09-25 18:13:56 +0200

    Revert "Added gang origin equipment availability and adjusted cost"

--- components/equipment.tsx
+++ components/equipment.tsx
@@ -480,7 +480,6 @@ const ItemModal: React.FC<ItemModalProps> = ({
     try {
       const requestBody: Record<string, any> = {
         gang_type_id: gangTypeId,
-        gang_id: gangId,  // Add gang_id for gang origin discounts/availability
         // Don't specify equipment_category to get ALL equipment
       };
 
@@ -524,6 +523,8 @@ const ItemModal: React.FC<ItemModalProps> = ({
       }
 
       const data: RawEquipmentData[] = await response.json();
+
+
 
       // Format and organize equipment by category
       const formattedData = data
