9996d67c225aae71acbc1eff2d674395e3e5f555
Johan Jönsson <jonsson.johan@outlook.com>
2025-10-06 11:23:19 +0200

    Deleted old migration files

--- supabase/migrations/20240416000000_add_vehicle_id_to_gang_logs.sql
+++
@@ -1,5 +0,0 @@
--- Add vehicle_id column to gang_logs table
-ALTER TABLE gang_logs ADD COLUMN vehicle_id UUID REFERENCES vehicles(id) ON DELETE CASCADE;
-
--- Add index for performance
-CREATE INDEX idx_gang_logs_vehicle_id ON gang_logs(vehicle_id); 
