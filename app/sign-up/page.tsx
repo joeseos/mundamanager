12bd14004c45c91966b753ded47ac3a35fdfd714
Johan Jönsson <jonsson.johan@outlook.com>
2025-11-11 19:35:19 +0100

    Moved the actions.ts to actions folder and renamed it to auth.ts

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { signUpAction } from "@/app/actions";
+import { signUpAction } from "@/app/actions/auth";
 import { FormMessage, Message } from "@/components/form-message";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
