cd3becb916d6c457fac8a0c64139bbba0224928f
Djidiouf <djidiouf@gmail.com>
2025-11-13 13:15:36 +1100

    Updated the project images and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,9 +47,10 @@ export const metadata = {
   },
   icons: {
     icon: [
-              { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon.ico', sizes: 'any' },
       { url: '/images/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/images/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/images/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
     apple: { url: '/images/apple-touch-icon.png' },
   },
