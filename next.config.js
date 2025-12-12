bd7fb1027cf3c72e807b712864b6cebaf26092c5
Djidiouf <djidiouf@gmail.com>
2025-12-13 04:57:11 +1100

    Updated packages React and Nextjs

--- next.config.js
+++ next.config.js
@@ -34,6 +34,7 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 2000],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    qualities: [75, 100],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
