1bc67dbff63506bd8aeec7729898ae496b49b8d6
joesoes <jonsson.johan@outlook.com>
2026-06-12 14:16:43 +0200

    Removed dead import usememo

--- components/customise/custom-equipment.tsx
+++ components/customise/custom-equipment.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { List, ListColumn, ListAction } from '@/components/ui/list';
 import { CustomEquipment } from '@/app/lib/customise/custom-equipment';
