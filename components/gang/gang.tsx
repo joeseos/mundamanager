e775e880bbc93a474e6b99ef3d8073384b91120f
joesoes <jonsson.johan@outlook.com>
2026-06-17 19:12:55 +0200

    Cleanup

--- components/gang/gang.tsx
+++ components/gang/gang.tsx
@@ -1052,7 +1052,7 @@ export default function Gang({
                   className="print:hidden"
                   title="Print Options"
                 >
-                  <Link href={`/gang/${id}/print`} prefetch={false}>
+                  <Link href={`/gang/${id}/print`}>
                     <FiPrinter className="w-5 h-5" />
                   </Link>
                 </Button>
