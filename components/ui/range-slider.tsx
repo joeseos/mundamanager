f5e5d60d23e99093234dec3e323d1108c189ed4b
Djidiouf <djidiouf@gmail.com>
2026-07-11 01:42:33 +1000

    Updated style of sliders

--- components/ui/range-slider.tsx
+++ components/ui/range-slider.tsx
@@ -41,18 +41,18 @@ export function RangeSlider({
         minStepsBetweenThumbs={0}
       >
         <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-muted">
-          <SliderPrimitive.Range className="absolute h-full bg-green-600" />
+          <SliderPrimitive.Range className="absolute h-full bg-primary" />
         </SliderPrimitive.Track>
 
         {/* First thumb (minimum value) */}
         <SliderPrimitive.Thumb
-          className="block h-4 w-4 rounded-full border border-border bg-card shadow-xs transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+          className="block h-4 w-4 rounded-full border border-muted-foreground bg-card shadow-xs transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
           aria-label="Set minimum value"
         />
 
         {/* Second thumb (maximum value) */}
         <SliderPrimitive.Thumb
-          className="block h-4 w-4 rounded-full border border-border bg-card shadow-xs transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+          className="block h-4 w-4 rounded-full border border-muted-foreground bg-card shadow-xs transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
           aria-label="Set maximum value"
         />
       </SliderPrimitive.Root>
