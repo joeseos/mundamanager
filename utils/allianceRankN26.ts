d3ff1e06d209b509c24b28f11976cfab7e961e45
Djidiouf <djidiouf@gmail.com>
2026-08-19 23:02:42 +1000

    Added Alliances ranking for new alliances of PDF

--- utils/allianceRankN26.ts
+++ utils/allianceRankN26.ts
@@ -1,3 +1,14 @@
 export const allianceRankN26: { [key: string]: number } = {
-  "justicar courts": 1,
+"psi-syndica": 1,
+"rogue factoria": 2,
+//
+"mercator pallidus": 10,
+"mercator nautica": 11,
+"mercator sanguis": 12,
+"justicar courts": 13,
+//
+"house greim": 20,
+"house ko'iron": 21,
+"house ty": 22,
+"the imperial house": 23,
 };
